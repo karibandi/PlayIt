67802f837ecc379c85f7b80359d3aeda11cac093
Surendra Karibandi <surendrakaribandi@gmail.com>
2021-07-18 08:02:04 +0530

    Initial Commit

---
+++ RizzleCodeChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RizzleCodeChallenge
+//
+//  Created by Surendra Karibandi on 18/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
